51a377c7f337798a2ade14f6d3cd8ba9c2951edc
Abisola Afolabi <afolabiadedoja@gmail.com>
2022-04-08 05:17:51 -0700

    Write a C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**
+/*
  *  main- prints the phrase "Programming is like building a multilingual puzzle"
  *  Return: 0 if exited properly, non-zero otherwise
  */
@@ -8,7 +8,7 @@ int main(void)
 
 {
 
-		puts("Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 
 }
