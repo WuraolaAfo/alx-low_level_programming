e7eeac88608fd7a89e208a922c6211e398c5d50a
Abisola Afolabi <afolabiadedoja@gmail.com>
2022-04-08 19:12:20 -0700

    program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 
 
 /**
-*main-program that prints all single digit numbers of base 10 starting from
+*main-program that prints all single digit numbers of base 10 starting
 *You are not allowed to use any variable of type char
 *You can only use the putchar function
 *Return: 0
@@ -12,23 +12,14 @@
 
 
 int main(void)
-
 {
 
-	int n = 48;
+	int i;
 
-	while (n <= 57)
+	for (i = 0; i <= 9; ++i)
+	putchar('0' + i);
 
-	{
-
-		putchar(n);
-
-		n += 1;
-
-	}
-
-	printf("\n");
+	putchar('\n');
 
 	return (0);
-
 }
