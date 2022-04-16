3bd8bb8bffa5c38e69efbf5aefe67a16bf403323
Abisola Afolabi <afolabiadedoja@gmail.com>
2022-04-15 17:32:03 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
-*print_times_table-print times table of the specified dimension
+*print_times_table- print times table of the specified dimension
 *@n: Dimension of times table
 *Return: void
 */
@@ -10,29 +10,42 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
-	int k;
+	int res;
 
-	if (n < 0 || n > 15)
+	if (!(n < 0 || n > 15))
 	{
-		for (j = 0; j <= n; ++j)
+		for (i = 0; i <= n; i++)
 		{
-			if (j == 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar('0');
-			}
-			else
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+			else if (res >= 10 && res < 100)
 			{
-				k = i * j;
-				_putchar((k < 100) ? ' ' : ('0' + k / 100));
-				_putchar((k <  10) ? ' ' : ('0' + k / 10 % 10));
-				_putchar('0' + k % 10);
-			}
-			if (j != n)
-			{
-				_putchar(',');
 				_putchar(' ');
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
 			}
-		}
+				else if (res >= 100 && j != 0)
+			{
+				_putchar((res / 100) + '0');
+				_putchar((res / 10) % 10 + '0');
+				_putchar((res % 10) + '0');
+			}
+				else
+					_putchar((res % 10) + '0');
+			}
 		_putchar('\n');
+		}
 	}
 }
