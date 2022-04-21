5a8f0c0c466ace68000920bb3a627bcfbbff8380
Abisola Afolabi <afolabiadedoja@gmail.com>
2022-04-20 17:39:13 -0700

    task 0 and task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*reset_to_98 - updates an integer's value to 98
+*@n: a pointer to the int that will be updated
+*Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
